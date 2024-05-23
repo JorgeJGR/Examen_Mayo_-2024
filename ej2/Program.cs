a9c2a54925aa4406b6aa3d3b8384d2b1b48f4d8c
JorgeJGR <jjguijarror@gmail.com>
2024-05-23 19:21:24 +0200

    Consulta_03 Ejercicio_02

--- ej2/Program.cs
+++ ej2/Program.cs
@@ -8,7 +8,17 @@ class Program
         string Generos
     );
 
-    static IEnumerable<Consulta3Dto> Consulta3() => // TODO
+    static IEnumerable<Consulta3Dto> Consulta3() 
+                                        => Cartelera.Peliculas
+                                        .Where(p => p.Generos.Contains("drama") ||
+                                                    p.Generos.Contains("romance") ||
+                                                    p.Generos.Contains("fantastico"))
+                                        .Select(p => new Consulta3Dto
+                                        (
+                                            p.Titulo,
+                                            p.Año,
+                                            $"[{string.Join(", ", p.Generos)}]"
+                                        ));                                                                      
        
     static void Main()
     {
@@ -16,7 +26,7 @@ class Program
             "Consulta 1: Mostrar titulo, año, país y valoración"
             + "\nde las películas españolas del año 2014"
             + "\nordenados descendentemente por valoración");
-        var peliculasEspañolas2014 = // TODO
+        var peliculasEspañolas2014 = "";
 
         Console.WriteLine(string.Join("\n", peliculasEspañolas2014) + "\n");
 
@@ -24,7 +34,7 @@ class Program
             "Consulta 2: Mostrar titulo, año, país y duración"
             + "\nde las películas con duración entre 100 y 120 minutos, es decir,"
             + "\ncon duración mayor o igual de 100 y menor o igual de 120.");
-        var peliculasDuracion = // TODO
+        var peliculasDuracion = "";
 
         Console.WriteLine(string.Join("\n", peliculasDuracion) + "\n");
 
@@ -36,20 +46,20 @@ class Program
         Console.WriteLine(
             "Consulta 4: Mostrar titulo, año y reparto"
             + "\nde las películas en las que conste un reparto con 2 actores");
-        var peliculasReparto = // TODO
+        var peliculasReparto = "";
 
         Console.WriteLine(string.Join("\n", peliculasReparto) + "\n");
 
         Console.WriteLine(
             "Consulta 5: Calcular la duración media de las películas de cada país."
             + "\nMostrar: \"Pais: DuracionMedia\"");
-        var duracionMedia = // TODO
+        var duracionMedia = "";
 
         Console.WriteLine(string.Join("\n", duracionMedia) + "\n");
 
         Console.WriteLine(
             "Consulta 6: Muestra sin repeticiones todos géneros de las peliculas en cartelera usando SelectMany.");
-        var generos = // TODO
+        var generos = "";
         
         Console.WriteLine($"[{string.Join(", ", generos)}]\n");            
     }
